ab6bdf8ffb139d4014208a60765e8c896fdd95ba
amrgitac <amrgitbackup@gmail.com>
2022-06-05 16:49:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Portfolio from './Portfolio';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Portfolio/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './firstpage/nav/Navbar';
+
+function Portfolio(){
+
+
+    return(<>
+        <div className='portfolio'>
+            <Navbar/>
+            
+        </div>
+    </>)
+}
+
+export default Portfolio;
