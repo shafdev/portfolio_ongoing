9935f7d8ac2907e8f0e4f69a82310bf2b69ac29e
amrgitac <amrgitbackup@gmail.com>
2022-06-07 13:48:07 +0530

    c7_07_06_22

--- src/firstpage/body/firstpage_components/Socials.js
+++ src/firstpage/body/firstpage_components/Socials.js
@@ -10,11 +10,21 @@ function Socials(props){
     <div className={true? "social-container":"social-container social-active"}>
     
     <div className={!props.context?"socials ":"socials social-active"}>
-        
+       <a href="https://github.com/shafdev/">
         <img src='logos/github.svg'/>
-        <img className="gmail-img" src='logos/gmail-logo.svg'/>
-        <img src='logos/insta.svg'/>
-        <img src='logos/twitter.svg'/>
+        </a> 
+        <a href="mailto: shafik8011@gmail.com">
+            <img className="gmail-img" src='logos/gmail-logo.svg'/>
+
+        </a>
+        <a href="https://www.instagram.com/shaf_ikk/">
+            <img src='logos/insta.svg'/>
+        </a>
+        
+        <a href="https://wa.me/7278892896">
+            <img  className="wa-img" src='logos/whatsapp.svg'/>
+        </a>
+        
 
     </div>
     </div>
